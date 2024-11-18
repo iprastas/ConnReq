03bf0894ff4d8181f363f192ab9098b64a48cd75
Anastasiia <iprastas@yandex.ru>
2024-11-18 17:04:42 +0300

    Edit header and footer, pages layout

--- ConnReq.WebUI/Controllers/HomeController.cs
+++ ConnReq.WebUI/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using ConnReq.Domain.Entities;
 using ConnReq.WebUI.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -5,7 +6,6 @@ using System.Diagnostics;
 
 namespace ConnReq.WebUI.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -15,12 +15,52 @@ namespace ConnReq.WebUI.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public ActionResult Index()
         {
+            UserSettings settings = new();
+            settings.Restore(Request);
+            if (settings != null)
+            {
+                switch (settings.TypeOfUser)
+                {
+                    case UserType.Customer:
+                        return RedirectToRoute(new
+                        {
+                            controller = "Customer",
+                            action = "Index",
+                            customer = settings.User
+                        });
+                    case UserType.Provider:
+                        return RedirectToRoute(new
+                        {
+                            controller = "Provider",
+                            action = "Index"
+                        });
+                    case UserType.Mgkx:
+                        return RedirectToRoute(new
+                        {
+                            controller = "Admin",
+                            action = "Index"
+                        });
+                    default:
+                        return RedirectToRoute(new
+                        {
+                            controller = "Account",
+                            action = "Login"
+                        });
+                }
+            }
             return View();
         }
 
-        public IActionResult Privacy()
+        public ActionResult About()
+        {
+            ViewBag.Message = "Краткое руководство.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
         {
             return View();
         }
